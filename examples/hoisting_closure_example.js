32bab0720d435a7c021c0544afef3eb67802ab09
Stephen Sebastin <stephenmail001@gmail.com>
2021-09-06 00:16:07 +0530

    currying example - user acitvity logging added

--- examples/hoisting_closure_example.js
+++ examples/hoisting_closure_example.js
@@ -1,7 +1,21 @@
+//currrying example
+
+var logging = function(date) {
+  return function(module) {
+    return function(userId) {
+      console.log("//Log:: INFO::  User :: "+userId+ " used module :: "+module + " on "+date);
+    }
+  }
+}
+
+var logModulewithDate = logging(new Date());
+
+
+
 // example scope 
 var country = "India"; //global scope
-
-function displayStateTN(){ // functional/local to the block/function scope
+let logUserIdwithModule = logModulewithDate( "Scope In JavaScript");
+function displayStateTN(userId){ // functional/local to the block/function scope
   
   let state="Tamilnadu";
   console.log("Country::"+country +" => State::"+state );
@@ -12,18 +26,22 @@ function displayStateTN(){ // functional/local to the block/function scope
     //a variable can be declared after it has been used. 
     //till declaring it will be considered as global variable
   console.log("Country::"+country +" => State::"+state + "=> City ::"+city );
-    
+      logUserIdwithModule(userId);
+      //logUserActivity();
   }
 //  console.log(city); //cannot be accessed
 displayStateTN.displayCity= displayCity;
+
 }
 
-displayStateTN();
-displayStateTN.displayCity();
+displayStateTN("101");
+displayStateTN.displayCity("101");
 
 
 
+logUserIdwithModule = logModulewithDate( "Closure In JavaScript");
 console.log("Closure example");
+
 //Closure
 // create a function that activates the inner closure when we need.
 //creating pain function for car, and it is called after payment is made
@@ -32,16 +50,27 @@ function car (paintColor) {
     console.log("Car color is :: " +paintColor+" now");
   }
 }
-console.log("Need to paint :: Color :: blue");
+console.log("101::: Need to paint :: Color :: blue");
 //gathering requirements 
 let paintCar = car("blue");
 //Getting cash
 console.log("Blue color paint price is 5000 ");
 paintCar(); //start painting
+      logUserIdwithModule("102");
+      
+      
+console.log("1003::: Need to paint :: Color :: Green");
+//gathering requirements 
+ paintCar = car("Green");
+//Getting cash
+console.log("Green color paint price is 5000 ");
+paintCar(); //start painting
+      logUserIdwithModule("1003");
 
 
-
-function hoistingExample() {
+function hoistingExample(userId) {
+  let logModulewithDate = logging(new Date("12-08-2021"));
+  let logUserIdwithModule = logModulewithDate("Hoisting in JavaScript");
     pen = "Ink Pen";
   console.log("It is "+pen);
   
@@ -55,13 +84,16 @@ function hoistingExample() {
   
 //defenitions will be hoised
   console.log("Old Price ::"+price);
+  
+  
+  logUserIdwithModule(userId);
 }
 
 
-hoistingExample();
-
+hoistingExample("1005");
+logUserIdwithModule = logModulewithDate("Higher Order Function in JavaScript");
 const students = [{"name":"Kumar","credits":"10"},{"name":"Krish","credits":"9"},{"name":"Tim","credits":"6"},{"name":"Steve","credits":"8"}];
-function higherOrderFunctionExampleWithArray(studentsList) {
+function higherOrderFunctionExampleWithArray(studentsList,userId) {
 //higher order fucntions
 
 var firstClassStudents = studentsList.filter(function(student) {
@@ -69,10 +101,10 @@ var firstClassStudents = studentsList.filter(function(student) {
 });
 
 console.log(firstClassStudents);
-
+logUserIdwithModule(userId);
 }
 
-higherOrderFunctionExampleWithArray(students);
+higherOrderFunctionExampleWithArray(students,"101");
 
 
 
@@ -101,5 +133,7 @@ let conveyMessage = convey('Jacob');
 conveyMessage("Welcome onboard...")
 conveyMessage('Start exploring the office','Jacob');
 
+
 }
 higherOrderFunctionExample();
+logUserIdwithModule("1004");
